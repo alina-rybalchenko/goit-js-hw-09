4f5897f1d5f136f3730e82ac51d4cedff36bab53
Alina <alinarybalchenko@hotmail.com>
2025-09-09 00:20:19 +0200

    initial files

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,31 @@
-console.log('Form');
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  formData = JSON.parse(savedData);
+  form.elements.email.value = formData.email || '';
+  form.elements.message.value = formData.message || '';
+}
+
+form.addEventListener('input', e => {
+  formData[e.target.name] = e.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  form.reset();
+  formData = { email: '', message: '' };
+  localStorage.removeItem(STORAGE_KEY);
+});
